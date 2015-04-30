abfa2dd79a7ae57446dddeaa12c6ccad4d62df98
Diego Kremer <diego.kremer@gmail.com>
2015-04-30 09:38:08 -0300

    #7 Ajustado icone e titulo

---
+++ br.com.miniware.alpha/src/br/com/miniware/alpha/TelaMostraResolucao.java
@@ -0,0 +1,43 @@
+package br.com.miniware.alpha;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TelaMostraResolucao {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaMostraResolucao window = new TelaMostraResolucao();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaMostraResolucao() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
